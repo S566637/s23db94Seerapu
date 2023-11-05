fca5e9f2cff84aa5bbddc0cd0fd6749d52b6146c
S566637 <142928309+S566637@users.noreply.github.com>
2023-11-04 19:38:37 -0500

    ./mfieupdated

--- controllers/costume.js
+++ controllers/costume.js
@@ -1,8 +1,19 @@
 var Costume = require('../models/costume');
 // List of all Costumes
-exports.costume_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume list');
-};
+// exports.costume_list = function(req, res) {
+// res.send('NOT IMPLEMENTED: Costume list');
+// };
+// List of all Costumes
+exports.costume_list = async function(req, res) {
+    try{
+    theCostumes = await Costume.find();
+    res.send(theCostumes);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // for a specific Costume.
 exports.costume_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
@@ -19,3 +30,5 @@ res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 exports.costume_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
 };
+
+
