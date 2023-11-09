af978cbbf0886b57d088955dff6058296163f8f2
S566637 <142928309+S566637@users.noreply.github.com>
2023-11-08 22:33:46 -0600

    ./mfileupated

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true});
+const connectionString = process.env.MONGO_CON;
+const mongoose = require('mongoose');
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
-var chooseRouter = require('./routes/choose');
+var chooseRouter = require('./routes/choose'); // Corrected the variable name here
 var boardRouter = require('./routes/board');
-var dogs = require("./models/dogs");
-var resourceRouter = require("./routes/resource");
+var resourceRouter = require('./routes/resource');
+const dogs = require("./models/dogs");
 
 var app = express();
 var db = mongoose.connection;
@@ -22,9 +22,9 @@ var db = mongoose.connection;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
 });
 
 app.use(logger('dev'));
@@ -35,19 +35,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dogs', dogsRouter)
-app.use('/choose', choose);
-app.use('/board',boardRouter);
-app.use('/resource',resourceRouter);
-
+app.use('/dogs', dogsRouter);
+app.use('/choose', chooseRouter); // Corrected the route usage here
+app.use('/board', boardRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -55,46 +54,46 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-  });
-  app.get('/dogs', (req, res) => {
-    // Fetch dogs data from a database or a predefined list
-    const dogs = [
-      { breed: 'Labrador', age: 3, name: 'Buddy' },
-      { breed: 'German Shepherd', age: 2, name: 'Max' },
-      // Add more dog objects as needed
-    ];
-  
-    res.render('dogs', {
-      title: 'Search Results - Dogs',
-      dogs: dogs,
-    });
-  });
+});
 
-  async function recreateDB(){
-    // Delete everything
-    await dogs.deleteMany();
-    let instance1 = new dogs({dogs_type:"ghost", size:'large', cost:15.4});
-    instance1.save().then(
-      doc=>{console.log("First object saved")}
-      ).catch(err=>{
-      console.error(err)
-      });
-let instance2 = new dogs({dogs_type:"ghost", size:'smlal', cost:15.0});
-    instance2.save().then(
-        doc=>{console.log("second object saved")}
-        ).catch(err=>{
-        console.error(err)
-        }); 
-let instance3 = new dogs({dogs_type:"ghost", size:'medium', cost:1.4});
-        instance3.save().then(
-          doc=>{console.log("Third object saved")}
-          ).catch(err=>{
-          console.error(err)
-          });             
-    }
-    let reseed = true;
-    if (reseed) {recreateDB();}
-  
+app.get('/dogs', (req, res) => {
+  // Fetch dogs data from a database or a predefined list
+  const dogs = [
+    { breed: 'Labrador', age: 3, name: 'Buddy' },
+    { breed: 'German Shepherd', age: 2, name: 'Max' },
+    // Add more dog objects as needed
+  ];
 
+  res.render('dogs', {
+    title: 'Search Results - Dogs',
+    dogs: dogs,
+  });
+});
+
+async function recreateDB() {
+  // Delete everything
+  await dogs.deleteMany();
+  let instance1 = new dogs({ dogs_type: "ghost", size: 'large', cost: 15.4 });
+  instance1.save().then(
+    doc => { console.log("First object saved") }
+  ).catch(err => {
+    console.error(err);
+  });
+  let instance2 = new dogs({ dogs_type: "ghost", size: 'smlal', cost: 15.0 });
+  instance2.save().then(
+    doc => { console.log("second object saved") }
+  ).catch(err => {
+    console.error(err);
+  });
+  let instance3 = new dogs({ dogs_type: "ghost", size: 'medium', cost: 1.4 });
+  instance3.save().then(
+    doc => { console.log("Third object saved") }
+  ).catch(err => {
+    console.error(err);
+  });
+}
+
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 module.exports = app;
