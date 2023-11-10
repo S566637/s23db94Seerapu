ea8c658e2b221b50ce0365976d8d6e625e2d01fc
S566637 <142928309+S566637@users.noreply.github.com>
2023-11-09 19:16:38 -0600

    ./mfileupated

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,105 +1,102 @@
-const mongoose = require('mongoose');
-const dogs = require('../models/dogs');
+var dogs = require('../models/dogs');
+
+exports.dogs_list = async function (req, res) {
+    res.send('NOT IMPLEMENTED: dogs list');
+};
+// for a specific dogs.
+exports.dogs_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: dogs detail: ' + req.params.id);
+};
+// Handle dogs create on POST.
+exports.dogs_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: dogs create POST');
+};
+// Handle dogs delete form on DELETE.
+exports.dogs_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: dogs delete DELETE ' + req.params.id);
+};
+// Handle dogs update form on PUT.
+exports.dogs_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: dogs update PUT' + req.params.id);
+};
 
 // List of all dogs
-exports.dogs_list = async function(req, res) {
+exports.dogs_list = async function (req, res) {
     try {
-        const thedogs = await dogs.find();
+        thedogs = await dogs.find();
         res.send(thedogs);
-    } catch (err) {
-        res.status(500).send({ error: `Update for ID ${req.params.id} failed. Error: ${err.message}` });
     }
-    
-};
-
-// Controller function for handling GET request for one dog
-exports.dogs_detail = async function(req, res) {
-    try {
-        // Convert the string representation of the ObjectId to an actual ObjectId
-        const dogId = mongoose.Types.ObjectId(req.params.id);
-
-        // Retrieve the dog details from the database
-        const dog = await dogs.findById(dogId);
-
-        if (!dog) {
-            // If the dog with the specified ID is not found
-            res.status(404).json({ message: `Dog with ID ${req.params.id} not found` });
-        } else {
-            // If the dog is found, send the details in the response
-            res.status(200).json(dog);
-        }
-    } catch (error) {
-        // Handle any errors that occur during the process
-        res.status(500).json({ error: `Error retrieving dog with ID ${req.params.id}` });
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
 };
-
-// Handle Costume create on POST
-exports.dogs_create_post = async function(req, res) {
-    console.log(req.body);
-    const document = new dogs();
-  
-    document.dogs_type = req.body.dogs_type;
-    document.cost = req.body.cost;
-    document.size = req.body.size;
-  
-    try {
-        const result = await document.save();
-        res.send(result);
-    } catch (err) {
-        res.status(500).send({ error: err.message });
-    }
-};
-
-// Handle Costume delete form on DELETE
-exports.dogs_delete = async function(req, res) {
-    try {
-        const dogId = req.params.id;
-        const deletedDog = await dogs.findByIdAndDelete(dogId);
-  
-        if (!deletedDog) {
-            return res.status(404).json({ message: 'Dog not found' });
-        }
-  
-        res.status(200).json({ message: 'Dog deleted successfully' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
-
-// Handle Costume update form on PUT
-// Handle Dog update form on PUT.
-// Handle Dog update form on PUT
-exports.dogs_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.id} with body
-    ${JSON.stringify(req.body)}`)
-    try {
-    let toUpdate = await dogs.findById( req.params.id)
-    // Do updates of properties
-    if(req.body.dogs_type)
-    toUpdate.dogs_type = req.body.dogs_type;
-    if(req.body.cost) toUpdate.cost = req.body.cost;
-    if(req.body.size) toUpdate.size = req.body.size;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
-    } catch (err) {
-    res.status(500)
-    res.send(`{"error": ${err}: Update for id ${req.params.id}
-    failed`);
-    }
-    };
-
-  
 
 // VIEWS
 // Handle a show all view
-exports.dogs_view_all_Page = async function(req, res) {
+exports.dogs_view_all_Page = async function (req, res) {
     try {
-        const thedogs = await dogs.find();
-        res.render('dogs', { title: 'Dogs Search Results', results: thedogs });
+        thedogs = await dogs.find();
+        res.render('dogs', { title: 'dogs Search Results', results: thedogs });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle dogs create on POST.
+exports.dogs_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new dogs();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.dogs_type = req.body.dogs_type;
+    document.cost = req.body.cost;
+    document.size = req.body.size;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+//New code for Assigment12 for screenshot1
+exports.dogs_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await dogs.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+//New code for Assignment 12 for screenshot2
+//Handle dogs update form on PUT
+exports.dogs_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await dogs.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.dogs_model)
+            toUpdate.dogs_model = req.body.dogs_model;
+        if (req.body.dogs_year) toUpdate.dogs_year = req.body.dogs_year;
+        if (req.body.dogs_price) toUpdate.dogs_price = req.body.dogs_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
     } catch (err) {
-        res.status(500).send({ error: err.message });
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
     }
 };
 
