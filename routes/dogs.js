959bbfd2b39789aa33b0943e01f6d63507f81608
S566637 <142928309+S566637@users.noreply.github.com>
2023-11-09 22:39:33 -0600

    ./mfileupated

--- routes/dogs.js
+++ routes/dogs.js
@@ -2,5 +2,5 @@ var express = require('express');
 const dogs_controlers= require('../controllers/dogs');
 var router = express.Router();
 /* GET dogs */
-router.get('/', dogs_controlers.dogs_view_all_Page );
+router.get('/dogs/:id', dogs_controller.dogs_detail);
 module.exports = router;
