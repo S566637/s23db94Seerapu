a8fd41690349090f174e248ae78ec6516f6b193e
S566637 <142928309+S566637@users.noreply.github.com>
2023-11-09 22:52:58 -0600

    ./mfileupated

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const dogs_controllers = require('../controllers/dogsControllers');
+const dogs_controllers = require('../controllers/dogsController');
 
 // Your other routes...
 
